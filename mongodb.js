e1811f325000c2c8e5a110df9b1e714106ab2820
miguelDalberto <migueldalberto.acc@gmail.com>
2021-05-04 15:03:29 -0300

    initial commit

---
+++ mongodb.js
@@ -0,0 +1,17 @@
+const {MongoClient, ObjectID} = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(
+  connectionURL, 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, 
+  (err, client) => {
+    if(err)return console.error(err)
+  
+    const db = client.db(databaseName)
+  
+})
