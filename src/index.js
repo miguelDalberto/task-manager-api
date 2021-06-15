90ef917a125662ac99628ed6cb7cbd5d48e2b2e3
miguelDalberto <migueldalberto.acc@gmail.com>
2021-06-14 22:22:09 -0300

    install and configure dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('./db/mongoose')
+require('dotenv').config()
 // import routers
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
