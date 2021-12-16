2a1b3f7a76edd0f783fca23d2960de896170d80d
miguelDalberto <migueldalberto.acc@gmail.com>
2021-12-16 16:05:28 -0300

    log requests and responses

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express = require('express')
+// start db
 require('./db/mongoose')
 // import routers
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+// import logger
+const logger = require('./logger')
+
+const NAMESPACE = 'SERVER'
 
 const app = express()
+
+// log requests and responses
+app.use((req, res, next) => {
+  logger.info(
+    NAMESPACE, 
+      `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`
+  )
+
+  res.on('finish', () => {
+    logger.info(
+      NAMESPACE,
+      `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - ` +
+      `IP: [${req.socket.remoteAddress}]`
+    )
+  })
+
+  next()
+})
+
 
 app.use(express.json())
 app.use(userRouter)
